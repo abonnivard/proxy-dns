7083217bc0f2c6573ca0c65d68b7bde8d666be2e
adrien <adri.bonnivard@gmail.com>
2024-11-26 23:20:53 +0100

    log rcode and full raw response data

--- logger.py
+++ logger.py
@@ -5,10 +5,11 @@ ES_HOST = "http://elasticsearch:9200/"
 es = Elasticsearch([ES_HOST])
 
 
-def full_log_request(response_data):
+def full_log_request(response_data, rcode):
     log_data = {
         "timestamp": datetime.utcnow(),
         "answers_count": response_data["answer"],
+        "rcode": rcode,
         "records": [],  # Liste pour contenir les logs détaillés de chaque enregistrement
     }
 
@@ -26,18 +27,18 @@ def full_log_request(response_data):
 
     # Indexation dans Elasticsearch
     es.index(index="proxy_logs_full", body=log_data)
-    print("Logged full request to Elasticsearch:", log_data)
 
 
-def log_request(response_data):
+def log_request(response_data, rcode):
     """
     Fonction pour logger une requête DNS dans Elasticsearch.
     """
-    full_log_request(response_data)
+    full_log_request(response_data, rcode)
 
     log_data = {
         "timestamp": datetime.utcnow(),
         "answers_count": response_data["answer"],
+        "rcode": rcode,
         "records": [],  # Liste pour contenir les logs détaillés de chaque enregistrement
     }
 
@@ -53,16 +54,17 @@ def log_request(response_data):
 
     # Indexation dans Elasticsearch
     es.index(index="proxy_logs", body=log_data)
-    print("Logged full request to Elasticsearch:", log_data)
 
 
-def log_error(error_message, source, data, query_data):
+def log_error(error_message, source, query_data_raw, query_data, answer_data):
     log_data = {
         "timestamp": datetime.utcnow(),
         "type": source,
         "error_message": str(error_message),
-        "data": data,
-        "query_data": query_data,
+        "query_data": query_data_raw,
+        "query_qname": query_data[0],
+        "query_type": query_data[1],
+        "query_class": query_data[2],
+        "answer_data": answer_data,
     }
     es.index(index="proxy_errors", body=log_data)
-    print("Logged error to Elasticsearch:", log_data)
